f57b2c31a5189cf0ad6a0f47f327012c0456ed09
Pashteto <dodonovpavel@gmail.com>
2021-08-23 01:34:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,70 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	//	"log"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
 )
 
+/*
+Напишите сервис для сокращения длинных URL.
+Требования:
+Сервер должен быть доступен по адресу: http://localhost:8080.
+Сервер должен предоставлять два эндпоинта: POST / и GET /{id}.
+Эндпоинт POST / принимает в теле запроса строку URL для сокращения и возвращает в ответ правильный сокращённый URL.
+Эндпоинт GET /{id} принимает в качестве URL параметра идентификатор сокращённого URL и возвращает ответ с кодом 307 и оригинальным URL в HTTP-заголовке Location.
+Нужно учесть некорректные запросы и возвращать для них ответ с кодом 400.
+*/
 func main() {
+
+	// адрес сервиса (как его писать, расскажем в следующем уроке)
+	endpoint := "http://localhost:8080/"
+	// контейнер данных для запроса
+	data := url.Values{}
+	// приглашение в консоли
+	fmt.Println("Введите длинный URL")
+	// открываем потоковое чтение из консоли
+	/*reader := bufio.NewReader(os.Stdin)
+	// читаем строку из консоли
+	long, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("\n", err)
+	}*/
+
+	fmt.Println("arsenal")
+	long := "https://practicum.yandex.ru/trainer/go-developer-beta/lesson/418fb6f9-9d16-41e1-87d1-efe7e78e6ab2/"
+	// заполняем контейнер данными
+	data.Set("url", long)
+	// конструируем HTTP-клиент
+	client := &http.Client{}
+	// конструируем запрос
+	// запрос методом POST должен, кроме заголовков, содержать тело
+	// тело должно быть источником потокового чтения io.Reader
+	// в большинстве случаев отлично подходит bytes.Buffer
+	request, err := http.NewRequest("POST", endpoint, bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		fmt.Println(err, "shit happens")
+	}
+	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	request.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+	// отправляем запрос и получаем ответ
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// печатаем код ответа
+	fmt.Println("Статус-код ", response.Status)
+	defer response.Body.Close()
+	// читаем поток из тела ответа
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// и печатаем его
+	fmt.Println(string(body))
 	fmt.Println("arsenal")
 }
