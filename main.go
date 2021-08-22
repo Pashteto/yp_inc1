a5582fbb7bb33255e8aee426539e47fd3648e9f7
Pashteto <dodonovpavel@gmail.com>
2021-08-23 00:49:53 +0300

    Update main.go

--- main.go
+++ main.go
@@ -2,65 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-)
-
-var numericKeyboard = tgbotapi.NewReplyKeyboard(
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("1"),
-		tgbotapi.NewKeyboardButton("2"),
-		tgbotapi.NewKeyboardButton("3"),
-	),
-	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("4"),
-		tgbotapi.NewKeyboardButton("5"),
-		tgbotapi.NewKeyboardButton("6"),
-	),
+	//	"log"
 )
 
 func main() {
-	fmt.Print("\n\n...nice\n")
-	//fmt.Println(fibb(33))
-	fmt.Print("\n\n...nice\n")
-
-	bot, err := tgbotapi.NewBotAPI("200517202:AAFCm1ZkhQ7FwUFipY91aiA_y51GziRAy9Y")
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-	fmt.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 666
-
-	updates, err := bot.GetUpdatesChan(u)
-	if err == nil {
-		for update := range updates {
-			if update.Message == nil { // ignore any non-Message Updates
-				continue
-			}
-
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			switch update.Message.Text {
-			case "open":
-				msg.ReplyMarkup = numericKeyboard
-			case "close":
-				msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
-			}
-
-			if _, err := bot.Send(msg); err != nil {
-				log.Panic(err)
-			}
-
-		}
-	}
+	fmt.Println("arsenal")
 }
