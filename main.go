7aae55c3c2775ba62e816eaa40a76a64b54280cc
Pashteto <dodonovpavel@gmail.com>
2021-08-23 19:48:38 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"net/url"
-	"strconv"
+	"os"
+	"os/signal"
 )
 
 /*
@@ -18,53 +18,53 @@ import (
 Эндпоинт GET /{id} принимает в качестве URL параметра идентификатор сокращённого URL и возвращает ответ с кодом 307 и оригинальным URL в HTTP-заголовке Location.
 Нужно учесть некорректные запросы и возвращать для них ответ с кодом 400.
 */
+
 func main() {
 
-	// адрес сервиса (как его писать, расскажем в следующем уроке)
-	endpoint := "http://localhost:8080/"
-	// контейнер данных для запроса
-	data := url.Values{}
-	// приглашение в консоли
-	fmt.Println("Введите длинный URL")
-	// открываем потоковое чтение из консоли
-	/*reader := bufio.NewReader(os.Stdin)
-	// читаем строку из консоли
-	long, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("\n", err)
-	}*/
+	// маршрутизация запросов обработчику
+	http.HandleFunc("/", HelloServer)
 
-	fmt.Println("arsenal")
-	long := "https://practicum.yandex.ru/trainer/go-developer-beta/lesson/418fb6f9-9d16-41e1-87d1-efe7e78e6ab2/"
-	// заполняем контейнер данными
-	data.Set("url", long)
-	// конструируем HTTP-клиент
-	client := &http.Client{}
-	// конструируем запрос
-	// запрос методом POST должен, кроме заголовков, содержать тело
-	// тело должно быть источником потокового чтения io.Reader
-	// в большинстве случаев отлично подходит bytes.Buffer
-	request, err := http.NewRequest("POST", endpoint, bytes.NewBufferString(data.Encode()))
-	if err != nil {
-		fmt.Println(err, "shit happens")
+	// конструируем свой сервер
+	server := &http.Server{
+		Addr: ":8080",
 	}
-	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	request.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-	// отправляем запрос и получаем ответ
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Println(err)
+	server.ListenAndServe()
+
+	/*	// маршрутизация запросов обработчику
+		http.HandleFunc("/", HelloServer)
+
+		http.ListenAndServe(":8080", nil)
+	*/
+
+	// создаём канал для перехвата сигналов OS
+	sigint := make(chan os.Signal, 1)
+	// перенаправляем сигналы OS в этот канал
+	signal.Notify(sigint, os.Interrupt)
+	// ожидаем сигнала
+	<-sigint
+	// получаем сигнал OS и начинаем процедуру «мягкого останова»
+	server.Shutdown(context.Background())
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!\n", r.URL.Path[1:])
+	//w.Write([]byte("<h1>Hello, World</h1>"))
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("<h1>M Get</h1>"))
+	case http.MethodPost:
+		w.Write([]byte("<h1>M Post</h1>"))
+		defer r.Body.Close()
+		// читаем поток из тела ответа
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// и печатаем его
+		fmt.Println(string(body))
+		fmt.Fprintf(w, "Hello, %v!\n", body)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
-	// печатаем код ответа
-	fmt.Println("Статус-код ", response.Status)
-	defer response.Body.Close()
-	// читаем поток из тела ответа
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// и печатаем его
-	fmt.Println(string(body))
-	fmt.Println("arsenal")
+
 }
