f18370eaed0968b9890dcb8a341babb7b95cfd46
Pashteto <dodonovpavel@gmail.com>
2021-08-25 15:44:30 +0300

    handlers and global var ALL works

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,46 +6,49 @@ import (
 	"io/ioutil"
 	"math/rand"
 	"net/http"
-	"strings"
+	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/gorilla/mux"
 )
 
-var rdb = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "", // no password set
-	DB:       0,  // use default DB
-})
-var ctxH, cancel = context.WithCancel(context.Background())
+var ctx, cancel = context.WithCancel(context.Background())
 
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println(err)
+type SpecificHandler struct {
+	Rdb redis.Client
+}
+
+func (h *SpecificHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+}
+
+func (h *SpecificHandler) GetHandler(w http.ResponseWriter, r *http.Request) {
+
+	id := mux.Vars(r)["key"]
+	//	fmt.Println(maped)
+	booid, _ := h.Rdb.Exists(ctx, id).Result()
+	// fmt.Println("key2", did)
+	if booid > 0 {
+		long_url, _ := h.Rdb.Get(ctx, id).Result()
+		http.Redirect(w, r, long_url, http.StatusTemporaryRedirect)
 	} else {
-		did, _ := rdb.Exists(ctxH, string(body)).Result()
-		fmt.Println("key2", did)
-		if did > 0 {
-			val2, _ := rdb.Get(ctxH, string(body)).Result()
-			http.Redirect(w, r, val2, http.StatusTemporaryRedirect)
-		} else {
-			http.NotFound(w, r)
-		}
+		http.NotFound(w, r)
 	}
-}
-func EmptyGetHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Method not allowed", http.StatusBadRequest)
-}
-func EmptyPostHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Method not allowed", http.StatusBadRequest)
-}
-func EmptyHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Add("google.com")
-	http.Redirect(w, r, "https://google.com", http.StatusTemporaryRedirect)
+
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
+/*func (h *SpecificHandler) EmptyGetHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Method not allowed", http.StatusBadRequest)
+}
+func (h *SpecificHandler) EmptyPostHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Method not allowed", http.StatusBadRequest)
+}*/
+func (h *SpecificHandler) EmptyHandler(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Add("google.com")
+	//	http.Redirect(w, r, "https://google.com", http.StatusTemporaryRedirect)
+	http.Error(w, "Method not allowed", http.StatusBadRequest)
+}
+
+func (h *SpecificHandler) PostHandler(w http.ResponseWriter, r *http.Request) {
 
 	var shorturl string
 	defer r.Body.Close()
@@ -54,27 +57,9 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		addthis := true
-		for it, it2 := range dataB {
-			if it == string(body) {
-				addthis = false
-				shorturl = strings.Join(it2, "")
-				break
-			}
-		}
-		if addthis {
-			shorturl = "http://localhost/" + fmt.Sprint((rand.Intn(1000)))
-
-			dataB.Set(string(body), shorturl)
-
-			fmt.Println(rdb)
-
-			//	fmt.Println(shorturl)
-		}
-		//fmt.Fprintf(w, "PostHandler, %v!\n", mux.Vars(r))
-		//fmt.Fprintf(w, "url data, %v!\n", r.URL.Path[1:])
+		id := fmt.Sprint((rand.Intn(1000)))
+		shorturl = "http://localhost/" + id
+		h.Rdb.Set(ctx, id, string(body), 1000*time.Second)
 	}
-	//fmt.Println(string(body))
-
 	fmt.Fprintf(w, "%v", shorturl)
 }
--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"io/ioutil"
-	"math/rand"
+
 	"net/http"
 	"os"
 	"os/signal"
-	"time"
+
+	"github.com/Pashteto/yp_inc1/handlers"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
@@ -16,62 +15,19 @@ import (
 
 var ctx = context.Background()
 
-//var dataB = url.Values{}
-
-var rdb = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "", // no password set
-	DB:       0,  // use default DB
-})
-
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["key"]
-	//	fmt.Println(maped)
-	booid, _ := rdb.Exists(ctx, id).Result()
-	// fmt.Println("key2", did)
-	if booid > 0 {
-		long_url, _ := rdb.Get(ctx, id).Result()
-		http.Redirect(w, r, long_url, http.StatusTemporaryRedirect)
-	} else {
-		http.NotFound(w, r)
-	}
-
-}
-
-func EmptyGetHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Method not allowed", http.StatusBadRequest)
-}
-func EmptyPostHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "Method not allowed", http.StatusBadRequest)
-}
-func EmptyHandler(w http.ResponseWriter, r *http.Request) {
-	//w.Header().Add("google.com")
-	http.Redirect(w, r, "https://google.com", http.StatusTemporaryRedirect)
-}
-
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-
-	var shorturl string
-	defer r.Body.Close()
-	// читаем поток из тела ответа
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		id := fmt.Sprint((rand.Intn(1000)))
-		shorturl = "http://localhost/" + id
-		rdb.Set(ctx, id, string(body), 10*time.Second)
-	}
-	fmt.Fprintf(w, "%v", shorturl)
-}
-
 func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	rdb.Set(ctx, "key0", "value0", 0)
+	sshand := handlers.SpecificHandler{Rdb: *rdb}
 
 	r := mux.NewRouter()
-	//	r.HandleFunc("/{key}/", GetHandler).Methods("GET")
-	r.HandleFunc("/{key}", GetHandler).Methods("GET")
-	r.HandleFunc("/", PostHandler).Methods("POST")
-	r.HandleFunc("/", EmptyHandler)
+	r.HandleFunc("/{key}", sshand.GetHandler).Methods("GET")
+	r.HandleFunc("/", sshand.PostHandler).Methods("POST")
+	r.HandleFunc("/", sshand.EmptyHandler)
 
 	http.Handle("/", r)
 
