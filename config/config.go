a42c5ba04cdbaa750dac960493573204d5cbecaf
Pashteto <dodonovpavel@gmail.com>
2021-09-15 17:28:03 +0300

    flagsv6

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"errors"
 	"os"
+	"strings"
 )
 
 type Config struct {
@@ -11,14 +13,31 @@ type Config struct {
 	FStorPath string `env:"FILE_STORAGE_PATH" envDefault:"${HOME}/go/src/github.com/Pashteto/yp_inc1/tmp/URLs" envExpand:"true"`
 }
 
-func (cfg *Config) UpdateByFlags(ServAddr, BaseURL, FStorPath, RedisPtr *string) (changed bool) {
-	if *ServAddr != ":8080" {
-		cfg.ServAddr = *ServAddr
-		changed = true
-	}
+func (cfg *Config) UpdateByFlags(ServAddr, BaseURL, FStorPath, RedisPtr *string) (changed bool, err error) {
+	changed = false
+	err = nil
 	if *BaseURL != "http://localhost:8080" {
 		changed = true
 		cfg.BaseURL = *ServAddr
+	}
+	if *ServAddr != ":8080" {
+		changed = true
+		cfg.ServAddr = *ServAddr
+		fjnv := strings.SplitAfter(cfg.ServAddr, ":")
+		var base string
+		var port string
+		if len(fjnv) > 0 {
+			port = fjnv[len(fjnv)-1]
+		}
+		fjnv = strings.SplitAfter(cfg.BaseURL, ":")
+		if len(fjnv) > 0 {
+			base = strings.Join(fjnv[:len(fjnv)-1], "")
+		}
+		if base == "" || port == "" {
+			err = errors.New("SERVER_ADDRESS, BASE_URL flags error")
+			return
+		}
+		cfg.BaseURL = base + port
 	}
 	if *FStorPath != os.Getenv("HOME") {
 		changed = true
