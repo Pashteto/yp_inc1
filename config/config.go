357aa5339c202d7385c3b70b987bcf303a303418
Pashteto <dodonovpavel@gmail.com>
2021-10-16 20:33:50 +0300

    test 12_3

--- config/config.go
+++ config/config.go
@@ -5,8 +5,8 @@ type Config struct {
 	BaseURL   string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FStorPath string `env:"FILE_STORAGE_PATH" envDefault:"../URLs" envExpand:"true"`
 
-	PostgresURL string `env:"DATABASE_URL" envDefault:"host=localhost port=5432 user=postgres password=kornkorn dbname=mydb sslmode=disable" envExpand:"true"`
-	// PostgresURL string `env:"DATABASE_URL" envDefault:"postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable"`
+	// PostgresURL string `env:"DATABASE_URL" envDefault:"host=localhost port=5432 user=postgres password=kornkorn dbname=mydb sslmode=disable" envExpand:"true"`
+	PostgresURL string `env:"DATABASE_URL" envDefault:"postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable"`
 }
 
 func (cfg *Config) UpdateByFlags(ServAddr, BaseURL, FStorPath, PostgresURL *string) (bool, error) {
@@ -23,8 +23,8 @@ func (cfg *Config) UpdateByFlags(ServAddr, BaseURL, FStorPath, PostgresURL *stri
 		changed = true
 		cfg.FStorPath = *FStorPath
 	}
-	if *PostgresURL != "host=localhost port=5432 user=postgres password=kornkorn dbname=mydb sslmode=disable" {
-		// if *PostgresURL != "postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable" {
+	// if *PostgresURL != "host=localhost port=5432 user=postgres password=kornkorn dbname=mydb sslmode=disable" {
+	if *PostgresURL != "postgres://postgres:postgres@postgres:5432/praktikum?sslmode=disable" {
 		cfg.PostgresURL = *PostgresURL
 	}
 	return changed, nil
